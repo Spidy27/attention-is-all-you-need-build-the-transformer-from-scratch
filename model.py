59bc7cf45b3e0ee894ff861689bc29ca43eb8562
Spidy27 <79884962+Spidy27@users.noreply.github.com>
2026-07-17 14:43:22 +0530

    Step 4: decode_ids_to_tokens

--- model.py
+++ model.py
@@ -47,8 +47,13 @@ def encode_sentence_to_ids(sentence, vocab, unk_token='<unk>'):
 
     return res
 
-# Step 4 - decode_ids_to_tokens (not yet solved)
-# TODO: implement
+# Step 4 - decode_ids_to_tokens
+def decode_ids_to_tokens(ids, id_to_token):
+    res = []
+    for id in ids:
+        res.append(id_to_token[id])
+
+    return res
 
 # Step 5 - pad_id_sequence (not yet solved)
 # TODO: implement
